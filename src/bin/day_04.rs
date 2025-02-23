f7a94ba06bdeaff904ae421e910b7412d16d57b0
Adrien Treuille <adrien.g.treuille@gmail.com>
2025-02-23 13:15:28 -0800

    Solved 4a

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -1,9 +1,55 @@
-use regex::Regex;
+use advent_of_code_2024_in_rust::grid;
+use itertools::{iproduct, Itertools};
+use ndarray::Array2;
 
 fn main() {
     // Parse the input
     let input = include_str!("../../puzzle_inputs/day_04.txt");
     println!("input:\n{}", input);
+
+    let input: Array2<char> = grid::parse_char_grid(input, |x| x);
+    println!("input:\n{:?}", input);
+    println!("input.dim(): {:?}", input.dim());
+    //let (w, h) = input.dim();
+
+    const DIRECTIONS: [(isize, isize); 8] = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ];
+    const TARGET: &str = "XMAS";
+    let iter = iproduct!(ndarray::indices(input.dim()), DIRECTIONS.iter());
+    let mut sum: usize = 0;
+    iter.for_each(|(idx, direction)| {
+        let (x, y) = (idx.0 as isize, idx.1 as isize);
+        //println!("x: {}, y: {}", x, y);
+        //println!("direction: {:?}", direction);
+
+        let found = TARGET.chars().enumerate().all(|(i, c)| {
+            let (dx, dy) = direction;
+            let x = x + i as isize * dx;
+            if x < 0 {
+                return false;
+            }
+            let y = y + i as isize * dy;
+            if y < 0 {
+                return false;
+            }
+            input.get((x as usize, y as usize)) == Some(&c)
+        });
+        if found {
+            sum += 1;
+        }
+    });
+
+    println!("sum: {}", sum);
+    //
+    //DIRECTIONS.iter().
 
     //let mul_regex = Regex::new(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     //let (mut sol_04a, mut sol_04b): (u32, u32) = (0, 0);
