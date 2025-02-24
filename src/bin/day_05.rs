a57232b234eecca4400451c73c0f4d140ea158eb
Adrien Treuille <adrien.g.treuille@gmail.com>
2025-02-24 10:22:51 -0800

    Using sort_unstable_by() to simplify day_05

--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -1,5 +1,4 @@
 use advent_of_code_2024_in_rust::parse_regex::parse_lines;
-use itertools::Itertools;
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
@@ -9,12 +8,17 @@ fn main() {
     let (ordering, pages) = input.split_once("\n\n").unwrap();
     let ordering_regex = Regex::new(r"(\d+)\|(\d+)").unwrap();
 
-    // Parse the ordering into the is_less_than function
+    // Parse the ordering into the compare(..) function
     let mut less_than: HashMap<u32, HashSet<u32>> = HashMap::new();
-    for (a, b) in parse_lines(ordering_regex.clone(), ordering) {
+    for (a, b) in parse_lines(ordering_regex, ordering) {
         less_than.entry(a).or_default().insert(b);
     }
-    let is_less_than = |a: &u32, b: &u32| less_than.get(a).unwrap().contains(b);
+    let compare = |a: &u32, b: &u32| match (a, b) {
+        (a, b) if a == b => std::cmp::Ordering::Equal,
+        (a, b) if less_than.get(a).unwrap().contains(b) => std::cmp::Ordering::Less,
+        (a, b) if less_than.get(b).unwrap().contains(a) => std::cmp::Ordering::Greater,
+        _ => panic!("Incomparable values: {a}, {b}"),
+    };
 
     // Parse the pages
     let pages: Vec<Vec<u32>> = pages
@@ -23,16 +27,13 @@ fn main() {
         .collect();
 
     // Solve the puzzles
-    let mut sol_05a: u32 = 0;
-    let mut sol_05b: u32 = 0;
-    let sorted = |page: &[u32]| page.iter().tuple_windows().all(|(a, b)| is_less_than(a, b));
-    for page in pages {
-        if sorted(&page) {
+    let (mut sol_05a, mut sol_05b) = (0, 0);
+    for mut page in pages {
+        if page.is_sorted_by(|a, b| compare(a, b) == std::cmp::Ordering::Less) {
             sol_05a += page[page.len() / 2];
         } else {
-            let sorted_page = sort(&page, is_less_than);
-            assert!(sorted(&sorted_page));
-            sol_05b += sorted_page[sorted_page.len() / 2];
+            page.sort_unstable_by(compare);
+            sol_05b += page[page.len() / 2];
         }
     }
 
@@ -49,15 +50,4 @@ fn main() {
     println!("My solution: {sol_05b}");
     println!("Correct solution: {correct_sol_05b}");
     println!("Equal: {:?}\n", sol_05b.cmp(&correct_sol_05b));
-}
-
-fn sort(page: &[u32], is_less_than: impl Fn(&u32, &u32) -> bool + Copy) -> Vec<u32> {
-    if page.len() <= 1 {
-        return page.to_vec();
-    }
-    let split_idx = page.len() / 2;
-    let (left, right) = page.split_at(split_idx);
-    let left = sort(left, is_less_than);
-    let right = sort(right, is_less_than);
-    left.into_iter().merge_by(right, is_less_than).collect()
 }
